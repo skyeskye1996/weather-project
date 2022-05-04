88053ba8fc59508101d4784f8e2063808a5dcfb2
skyeskye1996 <102100170+skyeskye1996@users.noreply.github.com>
2022-05-04 20:14:55 +0100

    New method to add in forecast for 5 days

--- src/index.js
+++ src/index.js
@@ -64,6 +64,36 @@ function searchSubmit(event) {
 let searchForm = document.querySelector("#city-form");
 searchForm.addEventListener("submit", searchSubmit);
 
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(forecastHTML);
+}
+
 function showWeather(response) {
   let city = response.data.name;
   let country = response.data.sys.country;
@@ -136,3 +166,4 @@ let currentLocation = document.querySelector("#current-location");
 currentLocation.addEventListener("click", getCurrentPosition);
 
 searchCity("London");
+displayForecast();
